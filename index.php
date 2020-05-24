3f61e8f6d36a39830a00c0cac6628f5221502998
cyrille <cyrilleotieno72gmail.com>
2020-05-24 20:31:28 +0300

    comming soon

--- index.php
+++ index.php
@@ -5,15 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Comming soon</title>
     <style>
+        *{
+            margin: 0;
+        }
         body,html{
-            height: 100;
+            height: 100%;
         }
         .bgimg{
             background-image:url('public/img/product_pic31.jpg');
             height: 100%;
             background-position: center;
             position: relative;
-            color: white;
+            color: black;
             font-family:'Courier New', Courier, monospace;
             font-size:25px;
         }
@@ -38,19 +41,22 @@
             margin: auto;
             width: 40%;
         }
+        #demo{
+            font-weight: bold;
+        }
     </style>
 </head>
 
 <body>
 <script>
-    var countDownDAte=new Date("may 24,2020 22:00:00");
+    var countDownDAte=new Date("may 24,2020 22:20:00");
     var x=setInterval(
         function(){
             var now=new Date().getTime();
             var distance=countDownDAte-now;
             var days=Math.floor(distance/(1000*60*60*24));
-            var hours=Math.floor(distance%(1000*60*60*24))/(1000*60*60);
-            var minutes=Math.floor(distance%(1000*60*60))/(1000*60);
+            var hours=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
+            var minutes=Math.floor((distance%(1000*60*60))/(1000*60));
             var seconds=Math.floor((distance%(1000*60))/1000);
             document.getElementById("demo").innerHTML=days+"d"+hours+"h"+minutes+"m"+seconds+"s";
             if(distance<0){
@@ -60,13 +66,15 @@
         },1000
     );
 </script>
-    <div class="classgbimg">
+    <div class="bgimg">
         <div class="topleft">
+	<p>Foodie</p>
         </div>
         <div class="middle">
         <h1>COMING SOON</h1>
         <hr>
-        <p>35 DAYS</p>
+        
+        <p id="demo">35 DAYS</p>
         </div>
         <div class="bottomleft">By cyrille</div>
     </div>
