a1619d552d6b935c3016fadcc401ad8298547fee
cyrille <cyrilleotieno72gmail.com>
2020-05-25 12:05:46 +0300

    second

--- public/html/login.php
+++ public/html/login.php
@@ -1,104 +1,111 @@
-<?php
-    if(isset($_GET['error'])){
-        $error=$_GET['error'];
-    }else{
-        $error=null;
-    }
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css"  media="screen and (min-width:600px)">
-    <link rel="stylesheet" media="screen and (max-width:600px)" href="../css/media.css" >
- 
-    <title>Home</title>
-    <script>
-        
-        function myFunction(){
-            var doc=document.getElementById('mainbar');
-            if(doc.style.display=="block"){
-                doc.style.display="none";
-               
-            }
-            else{
-                doc.style.display="block";
-                
-            }
-        }
-        function myFunction1(){
-            var doc=document.getElementById('cart_click');
-            if(doc.style.display=="block"){
-                doc.style.display="none";
-               
-            }
-            else{
-                doc.style.display="block";
-                
-            }
-        }
-        
-    </script> 
+	<title>Login V4</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href=".../img/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="../vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="../vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../css/util.css">
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
+<!--===============================================================================================-->
 </head>
 <body>
-       <div class="navsect">
-        <nav>
-            <div class="logo">
-                <img src="../img/icons/nf1_(4).jpg" alt="">
-            </div>
-                
-            <div id="mainbar">
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Account</a></li>
-                    <li><a href="">Order</a></li>
-                    <li><a href="">Contact Us</a></li>
-                </ul>
-            </div>
-            <div id="hambur">
-                <a href="javascript:void(0);" onClick="myFunction()"><i class="far fa-bars"></i>bar</a>
-            </div>
-        </nav>
-    </div>
-    <div class="loginpage">
-    <h1 class="logitle">Login</h1>
-    <form action="../handle/login_handle.php" method="post">
-        <label for="" class="logi">Email<input name="mail" type="text" placeholder="Email" required>
-        <p class="error"><?php
-            if($error=='missing'){
-                echo"Field cannot be left empty";
-            }elseif($error=='invalmail'){
-                echo"Invalid email Address";
-            }elseif ($error=='nouser') {
-                echo'Account does not exist';
-            }
-        
-        ?></p>
-        </label>
-        <label for="" class="logi">Password<input name="pwd" type="password" placeholder="Password" required>
-        <p class="error"><?php
-            if($error=='wrongpass'){
-                echo'wrong Password';
-            }
-        ?></p>
-        </label>
-        <button class="loginbut" name="loginn">Login</button>
-    </form>
-    <ul class="accounta">
-        <li><a href="">Forgot Password?</a></li>
-        <li><a href="signup.php">Don't have an account.</a></li>
-    </ul>
-    <h2><span>or</span></h2>
-    <div class="social">
-        <h5>Login with your social media account</h5>
-        <div class="socialcol">
-        <a href="" class="socialicons">Facebook</a>
-        <a href="" class="socialicons">Google</a>
-        <a href="" class="socialicons">Twitter</a>
-            </div>
-    </div>
-        
-    </div>
+	
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('../img/product_pic31.jpg');">
+			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+				<form class="login100-form validate-form" method="POST" action="../handle/login_handle.php">
+					<span class="login100-form-title p-b-49">
+						Login
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate="Email is required">
+						<span class="label-input100">Email</span>
+						<input class="input100" type="email" name="mail" placeholder="Type your Email">
+						<span class="focus-input100" data-symbol="&#xf190;"></span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
+						<span class="label-input100">Password</span>
+						<input class="input100" type="password" name="pwd" placeholder="Type your password">
+						<span class="focus-input100" data-symbol="&#xf190;"></span>
+					</div>
+					
+					<div class="text-right p-t-8 p-b-31">
+						<a href="signup.php">
+							Dont have an account?
+						</a>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn" name="loginn">
+								Login
+							</button>
+						</div>
+					</div>
+
+					<div class="txt1 text-center p-t-54 p-b-20">
+						<span>
+							Or Sign Up Using
+						</span>
+					</div>
+
+					<div class="flex-c-m">
+						<a href="#" class="login100-social-item bg1">
+							<i class="fa fa-facebook"></i>
+						</a>
+
+						<a href="#" class="login100-social-item bg2">
+							<i class="fa fa-twitter"></i>
+						</a>
+
+						<a href="#" class="login100-social-item bg3">
+							<i class="fa fa-google"></i>
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="../vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/bootstrap/js/popper.js"></script>
+	<script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/daterangepicker/moment.min.js"></script>
+	<script src="../vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="../js/main.js"></script>
+
 </body>
 </html>
--- public/html/signup.php
+++ public/html/signup.php
@@ -1,125 +1,117 @@
-<?php
-if(isset($_GET['error'])){
-    $error=$_GET['error'];
-}
-else{
-    $error="";
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style.css"  media="screen and (min-width:600px)">
-    <link rel="stylesheet" media="screen and (max-width:600px)" href="../css/media.css" >
- 
-    <title>Home</title>
-    <script>
-        
-        function myFunction(){
-            var doc=document.getElementById('mainbar');
-            if(doc.style.display=="block"){
-                doc.style.display="none";
-               
-            }
-            else{
-                doc.style.display="block";
-                
-            }
-        }
-        function myFunction1(){
-            var doc=document.getElementById('cart_click');
-            if(doc.style.display=="block"){
-                doc.style.display="none";
-               
-            }
-            else{
-                doc.style.display="block";
-                
-            }
-        }
-        
-    </script> 
+	<title>Login V4</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href=".../img/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="../vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../vendor/select2/select2.min.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="../vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="../css/util.css">
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
+<!--===============================================================================================-->
 </head>
 <body>
-       <div class="navsect">
-        <nav>
-            <div class="logo">
-                <img src="../img/icons/nf1_(4).jpg" alt="">
-            </div>
-                
-            <div id="mainbar">
-                <ul>
-                    <li><a href="">Home</a></li>
-                    <li><a href="">Account</a></li>
-                    <li><a href="">Order</a></li>
-                    <li><a href="">Contact Us</a></li>
-                </ul>
-            </div>
-            <div id="hambur">
-                <a href="javascript:void(0);" onClick="myFunction()"><i class="far fa-bars"></i>bar</a>
-            </div>
-        </nav>
-    </div>
-    <div class="loginpage">
-    <h1 class="logitle">Login</h1>
-    <form action="../handle/signup_handle.php" method="post">
-        <label for="" class="logi">Username<input type="text" placeholder="Username" name="uname"required>
-        <p class="error"><?php
-            if($error=='invaluser'){
-                echo"Invalid Username";
-            }
-            elseif($error=='usertaken'){    
-                echo"User name already taken";
-            }elseif($error=="missing"){
-                echo"Field cannot be left empty";
-            }
-        
-        ?></p>
-        </label>
-        <label for="" class="logi">Email<input type="text" placeholder="Email" name="umail" required>
-        <p class="error"><?php
-            if($error=='invalmail'){
-                echo"Enter a valid email address";
-            }
-            elseif($error=='missing'){
-                echo"This field connot be left empty";
-            }
-            elseif($error=='mailtaken'){
-                echo"Email already registered to another accouunt";
-            }
-        
-        ?></p>
-        </label>
-        <label for="" class="logi">Password<input type="password" placeholder="Password" name="pwd" required>
-            <p class="error">
-                <?php
-                if($error=="pwdlen"){
-                    echo"pasword must be more than 6 characters long";
-                }
-                elseif($error=='missing'){
-                    echo"Field cannot be left empty";
-                }
-                ?>
-            </p>
-            </label>
-        <button class="loginbut" name="loginbut">Login</button>
-    </form>
-    <ul class="accounta">
-        <li><a href="">Forgot Password?</a></li>
-        <li><a href="">Don't have an account.</a></li>
-    </ul>
-    <h2><span>or</span></h2>
-    <div class="social">
-        <h5>Login with your social media account</h5>
-        <div class="socialcol">
-        <a href="" class="socialicons">Facebook</a>
-        <a href="" class="socialicons">Google</a>
-        <a href="" class="socialicons">Twitter</a>
-            </div>
-    </div>
-        
-    </div>
+	
+	<div class="limiter">
+		<div class="container-login100" style="background-image: url('../img/product_pic31.jpg');">
+			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+				<form class="login100-form validate-form" method="POST" action="../handle/signup_handle.php">
+					<span class="login100-form-title p-b-49">
+						Signup
+					</span>
+
+					<div class="wrap-input100 validate-input m-b-23" data-validate = "Username is reauired">
+						<span class="label-input100">Username</span>
+						<input class="input100" type="text" name="uname" placeholder="Type your username">
+						<span class="focus-input100" data-symbol="&#xf206;"></span>
+					</div>
+
+					<div class="wrap-input100 validate-input" data-validate="Email is required">
+						<span class="label-input100">Email</span>
+						<input class="input100" type="email" name="umail" placeholder="Type your Email">
+						<span class="focus-input100" data-symbol="&#xf190;"></span>
+					</div>
+					<div class="wrap-input100 validate-input" data-validate="Password is required">
+						<span class="label-input100">Password</span>
+						<input class="input100" type="password" name="pwd" placeholder="Type your password">
+						<span class="focus-input100" data-symbol="&#xf190;"></span>
+					</div>
+					
+					<div class="text-right p-t-8 p-b-31">
+						<a href="login.php">
+							Already have an account?
+						</a>
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<div class="wrap-login100-form-btn">
+							<div class="login100-form-bgbtn"></div>
+							<button class="login100-form-btn" name="loginbut">
+								Signup
+							</button>
+						</div>
+					</div>
+
+					<div class="txt1 text-center p-t-54 p-b-20">
+						<span>
+							Or Sign Up Using
+						</span>
+					</div>
+
+					<div class="flex-c-m">
+						<a href="#" class="login100-social-item bg1">
+							<i class="fa fa-facebook"></i>
+						</a>
+
+						<a href="#" class="login100-social-item bg2">
+							<i class="fa fa-twitter"></i>
+						</a>
+
+						<a href="#" class="login100-social-item bg3">
+							<i class="fa fa-google"></i>
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="../vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/animsition/js/animsition.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/bootstrap/js/popper.js"></script>
+	<script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/daterangepicker/moment.min.js"></script>
+	<script src="../vendor/daterangepicker/daterangepicker.js"></script>
+<!--===============================================================================================-->
+	<script src="../vendor/countdowntime/countdowntime.js"></script>
+<!--===============================================================================================-->
+	<script src="../js/main.js"></script>
+
 </body>
 </html>
