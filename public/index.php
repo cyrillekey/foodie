6bd80c81ecaabf0b126683d0ad1ae6961f5cb9ca
cyrille <cyrilleotieno72gmail.com>
2020-05-26 07:19:34 +0300

    helloe

--- public/index.php
+++ public/index.php
@@ -165,7 +165,7 @@ else{
                  </a>';
 
                  }else{
-                    echo '<a href="handle/addtocart.php?pdid='.$row1['product_id'].' id="'.$row1['product_id'].'" class="addtocart" onClick="hello()">
+                    echo '<a href="handle/addtocart.php?pdid='.$row1['product_id'].'">
                     <button>Add to basket</button>
                  </a>';
                  }
