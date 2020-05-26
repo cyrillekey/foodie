2b8e047fc38113b31301e793a9d6935a379597e8
cyrille <cyrilleotieno72gmail.com>
2020-05-26 13:05:38 +0300

    why

---
+++ public/html/send.php
@@ -0,0 +1,40 @@
+<?php
+require('../../vendor/autoload.php');
+use SendGrid\Mail\To;
+use SendGrid\Mail\Cc;
+use SendGrid\Mail\Bcc;
+use SendGrid\Mail\From;
+use SendGrid\Mail\Content;
+use SendGrid\Mail\Mail;
+use SendGrid\Mail\Personalization;
+use SendGrid\Mail\Subject;
+use SendGrid\Mail\Header;
+use SendGrid\Mail\CustomArg;
+use SendGrid\Mail\SendAt;
+use SendGrid\Mail\Attachment;
+use SendGrid\Mail\Asm;
+use SendGrid\Mail\MailSettings;
+use SendGrid\Mail\BccSettings;
+use SendGrid\Mail\SandBoxMode;
+use SendGrid\Mail\BypassListManagement;
+use SendGrid\Mail\Footer;
+use SendGrid\Mail\SpamCheck;
+use SendGrid\Mail\TrackingSettings;
+use SendGrid\Mail\ClickTracking;
+use SendGrid\Mail\OpenTracking;
+use SendGrid\Mail\SubscriptionTracking;
+use SendGrid\Mail\Ganalytics;
+use SendGrid\Mail\ReplyTo;
+$from = new SendGrid\Email(null, "cyrilleotieno7@gmail.com");
+$subject = "Hello World from the SendGrid PHP Library!";
+$to = new SendGrid\Email(null, "cyrilleotieno83@gmail.com");
+$content = new SendGrid\Content("text/plain", "Hello, Email!");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+$apiKey = getenv('SENDGRID_API_KEY');
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
