d24bbcb9f839f29e0a22181a90435b13ab3d524e
cyrille <cyrilleotieno72gmail.com>
2020-05-26 08:48:28 +0300

    mailer

---
+++ public/html/send_invoice.php
@@ -0,0 +1,13 @@
+<?php
+require_once '../../vendor/autoload.php';
+$transport=(new Swift_SmtpTransport('smtp.gmail.com',456,"ssl") )
+->setUSername('cyrilleotieno7@gmail.com'
+->setPassword('STACYM456'));
+$mailer=new Swift_Mailer($transport);
+$message=(new Swift_Message("Invoice"))
+    ->setFrom(array('cyrilleotieno7@gmail.com'=>'Cyrille'))
+    ->setTo(array('cyrilleotieno83@gmail.com'))
+    ->setBody("hello it worked");
+    $result=$mailer->send($message);
+    
+;
